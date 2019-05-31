0c4ba185bdd3ca16ff662a5b5355d739196a979a
Eric Yanmin Xu <ericxu0@stanford.edu>
2019-05-30 23:45:43 -0700

    i messed up

--- data/generate_data.py
+++ data/generate_data.py
@@ -9,7 +9,7 @@ def generate_linear_data(a, b, filename):
     for x in data:
         f.write("%.10f\n" % x)
 
-def generate_linear_data(mean, std, filename):
+def generate_normal_data(mean, std, filename):
     data = [np.random.normal(mean,std) for _ in range(N)]
     data.sort()
     f = open(filename, "w")
@@ -18,8 +18,8 @@ def generate_linear_data(mean, std, filename):
 
 def main():
     generate_linear_data(2, 1, "linear_a=2_b=1.txt")
-    generate_linear_data(1, 1, "normal_mean=1_std=1.txt")
-    generate_linear_data(2, 0.001, "normal_mean=2_std=0.001.txt")
+    generate_normal_data(1, 1, "normal_mean=1_std=1.txt")
+    generate_normal_data(2, 0.001, "normal_mean=2_std=0.001.txt")
 
 if __name__ == "__main__":
     main()
