a695f1f3d561523b39a933cedffac7fe12782457
Eric Yanmin Xu <ericxu0@stanford.edu>
2019-06-04 03:16:14 -0700

    finally lognormal works

--- calibrated_rmi.py
+++ calibrated_rmi.py
@@ -17,7 +17,6 @@ output_dir = "results/"
 quantiles_dir = "quantiles/"
 
 CSV_COLUMNS = ["feature" + str(i) for i in range(1, NUM_FEATURES + 1)] + ["value"]
-CSV_COLUMNS_EXPERT = CSV_COLUMNS#["feature" + str(i) for i in range(1, NUM_FEATURES + 1)] + ["stage1", "value"]
 
 def get_test_input_fn():
   return get_input_fn(data_dir + ".test", batch_size=10000, num_epochs=1, shuffle=False)
@@ -29,10 +28,10 @@ def get_train_input_fn(batch_size=10000, num_epochs=1, shuffle=False):
   return get_input_fn(data_dir + ".train", batch_size, num_epochs, shuffle)
 
 def get_test_expert_input_fn(idx):
-  return get_expert_input_fn(data_dir + "_expert" + str(idx) + ".test", batch_size=10000, num_epochs=1, shuffle=False)
+  return get_input_fn(data_dir + "_expert" + str(idx) + ".test", batch_size=10000, num_epochs=1, shuffle=False)
 
 def get_train_expert_input_fn(idx, batch_size=10000, num_epochs=1, shuffle=False):
-  return get_expert_input_fn(data_dir + "_expert" + str(idx) + ".train", batch_size, num_epochs, shuffle)
+  return get_input_fn(data_dir + "_expert" + str(idx) + ".train", batch_size, num_epochs, shuffle)
 
 def get_input_fn(file_path, batch_size, num_epochs, shuffle):
   df_data = pd.read_csv(
@@ -50,29 +49,9 @@ def get_input_fn(file_path, batch_size, num_epochs, shuffle):
       num_epochs=num_epochs,
       num_threads=1)
 
-def get_expert_input_fn(file_path, batch_size, num_epochs, shuffle):
-  df_data = pd.read_csv(
-      tf.gfile.Open(file_path),
-      names=CSV_COLUMNS_EXPERT,
-      skipinitialspace=True,
-      engine="python",
-      skiprows=1)
-  labels = df_data["value"]
-  return tf.estimator.inputs.pandas_input_fn(
-      x=df_data,
-      y=labels,
-      batch_size=batch_size,
-      shuffle=shuffle,
-      num_epochs=num_epochs,
-      num_threads=1)
-
 def create_feature_columns():
   # Categorical features.
   return [tf.feature_column.numeric_column("feature" + str(i)) for i in range(1, NUM_FEATURES + 1)]
-
-def create_expert_feature_columns():
-  # Categorical features.
-  return [tf.feature_column.numeric_column("feature" + str(i)) for i in range(1, NUM_FEATURES + 1)]# + [tf.feature_column.numeric_column("stage1")]
 
 def create_quantiles(quantiles_dir):
     """Creates quantiles directory if it doesn't yet exist."""
@@ -83,15 +62,6 @@ def create_quantiles(quantiles_dir):
         feature_columns=create_feature_columns(),
         num_steps=None)
 
-def create_expert_quantiles(idx, quantiles_dir):
-    """Creates quantiles directory if it doesn't yet exist."""
-    input_fn = get_test_expert_input_fn(idx)
-    tfl.save_quantiles_for_keypoints(
-        input_fn=input_fn,
-        save_dir=quantiles_dir,
-        feature_columns=create_expert_feature_columns(),
-        num_steps=None)
-
 def create_calibrated_linear(feature_columns, config, quantiles_dir):
     feature_names = [fc.name for fc in feature_columns]
     hparams = tfl.CalibratedLinearHParams(
@@ -99,21 +69,6 @@ def create_calibrated_linear(feature_columns, config, quantiles_dir):
                 num_keypoints=1000,
                 learning_rate=0.01)
     hparams.set_feature_param("feature1", "monotonicity", 1)
-    return tfl.calibrated_linear_regressor(
-            feature_columns=feature_columns,
-            model_dir=config.model_dir,
-            config=config,
-            hparams=hparams,
-            quantiles_dir=quantiles_dir)
-
-def create_calibrated_expert_linear(feature_columns, config, quantiles_dir):
-    feature_names = [fc.name for fc in feature_columns]
-    hparams = tfl.CalibratedLinearHParams(
-                feature_names=feature_names,
-                num_keypoints=1000,
-                learning_rate=0.01)
-    hparams.set_feature_param("feature1", "monotonicity", 1)
-    #hparams.set_feature_param("stage1", "monotonicity", 1)
     return tfl.calibrated_linear_regressor(
             feature_columns=feature_columns,
             model_dir=config.model_dir,
@@ -145,11 +100,6 @@ def create_estimator(config, quantiles_dir):
         return create_calibrated_lattice(feature_columns, config, quantiles_dir)
     else:
         return create_calibrated_linear(feature_columns, config, quantiles_dir)
-
-def create_expert_estimator(config, quantiles_dir):
-    """Creates estimator for given configuration based on --model_type."""
-    feature_columns = create_expert_feature_columns()
-    return create_calibrated_expert_linear(feature_columns, config, quantiles_dir)
 
 def calculate_collision_rate(estimator, input_fn, extension, N):
     data = [] 
@@ -211,7 +161,6 @@ def reset():
     os.mkdir(output_dir)
 
 def main():
-    '''
     reset()
     create_quantiles(quantiles_dir)
 
@@ -226,14 +175,11 @@ def main():
     #calculate_collision_rate(estimator, get_val_input_fn(), 20000)
     print("test stats:")
     calculate_collision_rate(estimator, get_test_input_fn(), ".test", 20000)
-    '''
     
     train_buckets, test_buckets = defaultdict(int), defaultdict(int)
+    final_output = open("final_predictions.txt", "w")
     for expert in range(10):
-        reset()
-        create_expert_quantiles(expert, quantiles_dir)
-        config = tf.estimator.RunConfig().replace(model_dir=output_dir)
-        cur_estimator = create_expert_estimator(config, quantiles_dir)
+        cur_estimator = create_estimator(config, quantiles_dir)
         cur_estimator.train(input_fn=get_train_expert_input_fn(expert, batch_size=32, num_epochs=1, shuffle=True))
 
         results = cur_estimator.predict(input_fn=get_train_expert_input_fn(expert))
@@ -243,10 +189,16 @@ def main():
             train_buckets[bucket] += 1
             
         results = cur_estimator.predict(input_fn=get_test_expert_input_fn(expert))
+        final_pred = []
         N = 20000
         for result in results:
             bucket = min(N - 1, max(0, int(result['predictions'][0]*N)))
+            final_pred.append(result['predictions'][0])
             test_buckets[bucket] += 1
+        final_pred.sort()
+        final_output.write(" ".join(["%.10f" % x for x in final_pred]) + "\n")
+
+    final_output.close()
 
     num_collisions, buckets_used, avg = 0, 0, 0
     cnt = Counter()
@@ -275,7 +227,17 @@ def main():
             #print("bucket %d, number of entries: %d" % (i, buckets[i]))
 
     print("test collision rate:", num_collisions/buckets_used)
+    print("fraction of buckets used:", buckets_used/20000)
     print("average number of entries in a bad bucket:", avg/num_collisions)
     print(cnt)
+    x, y = [], []
+    for e in cnt:
+        x.append(e)
+        y.append(cnt[e])
+    plt.bar(x, y)
+    plt.xlabel("Number of Elements in a Bucket")
+    plt.ylabel("Number of Buckets")
+    plt.title("Element Distribution in Buckets for Learned Index: lognormal.test")
+    plt.savefig("lognormal.png")
 
 main()
